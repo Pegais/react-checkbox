7ea04a7990db754257af030169212c90737a845e
Pegais <snehal9140@gmail.com>
2022-04-12 11:46:54 +0530

    1 file modified, 1 added

--- src/utils/Main.js
+++ src/utils/Main.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
+import object from "./Object.js"
 
 export default function Main({obj}){
+  let [product,setProduct]=useState(object)
+  console.log(object)
 console.log("this is from main ",obj)
+
+
+
+   
 
   return(
     <>
-    {
-      obj.map((ele)=>{
-        return <div key={ele.id}>{ele.value}</div>
-      })
-    }
+   
     </>
   )
 }
---
+++ src/utils/Object.js
@@ -0,0 +1,24 @@
+let obj=[
+  {
+    id:"1",
+    category:"pant",
+
+  },{
+    id:"2",
+    category:"shirt"
+  }
+  ,{
+    id:"3",
+    category:"shirt"
+  }, {
+    id:"4",
+    category:"pant",
+
+  }
+
+
+
+
+]
+
+export default obj;
